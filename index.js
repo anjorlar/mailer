4cf2d3f375b81e9d9c082baad9c566341107aade
anjorlar <adeanjola@gmail.com>
2019-08-12 15:17:27 +0100

    updated index file

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ hi: 'how are you' })
+  res.send({ WHAT: ' A WAWU' })
 })
 
 const PORT = process.env.PORT || 5000;
